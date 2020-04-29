ce17c31888bd6f7266bd5358f2703b4c26330bbe
OlhaKlymas <olhaklymas@gmail.com>
2020-04-29 19:20:40 +0300

    add

--- js/index.js
+++ js/index.js
@@ -107,33 +107,6 @@ else {
 	}
 }
 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
 $(window).on("load", function () {
 	var sum=0;
 	$('.banner-container li img').each(function(){
