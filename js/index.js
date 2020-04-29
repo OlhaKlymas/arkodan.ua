e204dbb201bc8d88f7d40c64736d0dd7881c97f5
OlhaKlymas <olhaklymas@gmail.com>
2020-04-29 19:02:54 +0300

    add js

--- js/index.js
+++ js/index.js
@@ -106,3 +106,58 @@ else {
 	  elems[i].classList.add("active-wide");
 	}
 }
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+$(window).on("load", function () {
+	var sum=0;
+	$('.banner-container li img').each(function(){
+		sum += $(this).width();
+	});
+	$('.banner-container ul').width(sum);
+});
+$(function(){
+	var winWidth = $(".banner-container").width();
+	var ulWidthCount = 0;
+	ulWidthCount = $('.banner-container li').length;
+	$(".banner-container li").width(winWidth/ulWidthCount);
+	$(".banner-container li").hover(function(){
+		ulWidthCount = $('.banner-container li').length;
+		var imgWidth = $(this).find("img").width();
+		var bannerLi = winWidth - imgWidth;
+		var remWidth = ulWidthCount - 1;
+		var appWidth = bannerLi/remWidth;
+		$(".banner-container li").stop(true, false).animate({width: appWidth},700);
+		$(this).stop(true, false).animate({width: imgWidth},700);
+		$(this).find("span.overlay").stop(true, false).fadeOut();
+	}, function(){
+		$(this).animate({width: winWidth/ulWidthCount},700);
+		$(".banner-container li").animate({width:winWidth/ulWidthCount},700);
+		$(this).find("span.overlay").fadeIn();
+	});
+});    
