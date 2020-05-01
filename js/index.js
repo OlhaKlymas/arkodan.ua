458ec083232e6dc94e82c8bcf686407daac95e6d
OlhaKlymas <olhaklymas@gmail.com>
2020-05-01 12:46:29 +0300

    add

--- js/index.js
+++ js/index.js
@@ -147,7 +147,7 @@ function ImgHover() {
 					$(".photo-wrap").children(i+1).toggleClass('right');
 				}
 				else if(i < 3){
-					$(".photo-wrap div").children(i+1).toggleClass('left');
+					$(".photo-wrap").children(i+1).toggleClass('left');
 				}
 			}
 
